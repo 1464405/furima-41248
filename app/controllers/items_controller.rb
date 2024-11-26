31de838f6b40c4b793a196d7602b32c083575d18
SHUN　YAMAZAKI <112524165+1464406@users.noreply.github.com>
2024-11-26 20:25:43 +0900

    商品一覧表示機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,10 @@
 class ItemsController < ApplicationController
   before_action :set_item, only: [:edit, :show, :update, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
+  def index
+    @items = Item.includes(:user).order('created_at DESC')
+  end
+
   def new
     @item = Item.new
   end
