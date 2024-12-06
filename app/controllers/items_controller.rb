37b2e40ba620b80b70c089f363daadc29da1ab30
SHUN　YAMAZAKI <112524165+1464406@users.noreply.github.com>
2024-12-06 16:49:35 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,7 +23,7 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    return if current_user.id == @item.user.id && !@item.order
+    return if current_user.id == @item.user.id && !@item.orde
 
     redirect_to action: :index
   end
