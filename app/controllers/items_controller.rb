d3d3f67ee04c3b9f570bcf6e93d235c50ca6ff27
SHUN　YAMAZAKI <112524165+1464406@users.noreply.github.com>
2024-12-06 18:05:08 +0900

    商品購入機能の査収。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -37,10 +37,12 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    return if current_user.id == @item.user.id
-
-    @item.destroy
-    redirect_to root_path
+    if current_user.id == @item.user.id
+      @item.destroy
+      redirect_to root_path
+    else
+      render 'show'
+    end
   end
 
   private
