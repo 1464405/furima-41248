e3f0570a70cdada30de1addaa813fa948e1e3269
SHUN　YAMAZAKI <112524165+1464406@users.noreply.github.com>
2024-12-06 16:55:11 +0900

    商品購入機能を査収致しました。

--- spec/factories/order_addresses.rb
+++ spec/factories/order_addresses.rb
@@ -1,9 +1,5 @@
 FactoryBot.define do
   factory :order_address do
-    transient do
-      user { FactoryBot.create(:user) }
-      item { FactoryBot.create(:item) }
-    end
     token             { 'tok_abcdefghijk00000000000000000' }
     post_code         { '000-0000' }
     prefecture_id     { rand(2..48) }
@@ -11,7 +7,5 @@ FactoryBot.define do
     street_address    { Faker::Lorem.word }
     building          { Faker::Lorem.word }
     telephone_number  { '09012341234' }
-    user_id           { user.id }
-    item_id           { item.id }
   end
 end
